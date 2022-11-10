df4d988375f08b4df434d8218a91fea8e6fa3620
Jack <jack.morgan.edwards@gmail.com>
2022-11-10 17:44:03 -0500

    restructured

---
+++ controllers/tasks.js
@@ -0,0 +1,69 @@
+const tasksRouter = require("express").Router();
+const Task = require("../models/task");
+const uniqid = require("uniqid");
+
+tasksRouter.get("/:id", (request, response, next) => {
+  Task.findById(request.params.id)
+    .then((task) => {
+      if (task) {
+        response.json(task);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+tasksRouter.get("/", (request, response) => {
+  Task.find({}).then((tasks) => {
+    response.json(tasks);
+  });
+});
+
+tasksRouter.delete("/:id", (request, response) => {
+  Task.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+tasksRouter.post("/", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const task = new Task({
+    content: body.content,
+    streak_start: new Date(),
+    updated_last: "1970",
+    id: uniqid(),
+  });
+
+  task.save().then((savedTask) => {
+    response.json(savedTask);
+  });
+});
+
+tasksRouter.put("/:id", (request, response, next) => {
+  const body = request.body;
+
+  const task = {
+    content: body.content,
+    streak_start: body.streak_start,
+    updated_last: body.updated_last,
+    id: body.id,
+  };
+
+  Task.findByIdAndUpdate(request.params.id, task, { new: true })
+    .then((updatedTask) => {
+      response.json(updatedTask);
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = tasksRouter;
--- index.js
+++ index.js
@@ -1,101 +1,10 @@
-require("dotenv").config();
-const express = require("express");
-const uniqid = require("uniqid");
-const cors = require("cors");
-const app = express();
-const Task = require("./models/task");
+const app = require("./app"); // the actual Express application
+const http = require("http");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-app.use(express.static("build"));
-app.use(cors());
-app.use(express.json());
+const server = http.createServer(app);
 
-app.get("/api/tasks/:id", (request, response, next) => {
-  Task.findById(request.params.id)
-    .then((task) => {
-      if (task) {
-        response.json(task);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch((error) => next(error));
-});
-
-app.get("/api/tasks", (request, response) => {
-  Task.find({}).then((tasks) => {
-    response.json(tasks);
-  });
-});
-
-app.get("/", (request, response) => {
-  response.send("<h1>Go to /api/tasks</h1>");
-});
-
-app.delete("/api/tasks/:id", (request, response) => {
-  Task.findByIdAndRemove(request.params.id)
-    .then((result) => {
-      response.status(204).end();
-    })
-    .catch((error) => next(error));
-});
-
-app.post("/api/tasks", (request, response) => {
-  const body = request.body;
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
-
-  const task = new Task({
-    content: body.content,
-    streak_start: new Date(),
-    updated_last: "1970",
-    id: uniqid(),
-  });
-
-  task.save().then((savedTask) => {
-    response.json(savedTask);
-  });
-});
-
-app.put("/api/tasks/:id", (request, response, next) => {
-  const body = request.body;
-
-  const task = {
-    content: body.content,
-    streak_start: body.streak_start,
-    updated_last: body.updated_last,
-    id: body.id,
-  };
-
-  Task.findByIdAndUpdate(request.params.id, task, { new: true })
-    .then((updatedTask) => {
-      response.json(updatedTask);
-    })
-    .catch((error) => next(error));
-});
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
-
-app.use(unknownEndpoint);
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  }
-
-  next(error);
-};
-
-app.use(errorHandler);
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
